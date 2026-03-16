5e1c0e35c429e1fbc3220872be9b3711f4485d84
Furkó Norbert <furkonorbert16@gmail.com>
2026-03-16 13:25:39 +0100

    hamburg

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -168,7 +168,7 @@ export default function Navbar() {
                     setTimeout(() => {
                       setLoading(false);
                       setLogoutMessage("Sikeres kijelentkezés!");
-                      setTimeout(() => setLogoutMessage("");, 3000);
+                      setTimeout(() => setLogoutMessage(""), 3000);
                     }, 1200);
                   }} />
                 </li>
